c58424e7359fd8e75469f0e7a0b71d5287e6806c
banalbat666 <114973120+banalbat666@users.noreply.github.com>
2022-12-06 15:13:13 -0500

    Add files via upload

---
+++ pages/about.js
@@ -0,0 +1,27 @@
+import Link from 'next/link';
+import Head from 'next/head';
+import styles from '../styles/Home.module.css'
+
+export default function AboutPage() {
+    return (
+        <div className={styles.main}>
+            <Head>
+                <title>About [Redacted]</title>
+            </Head>
+            <h1 id={styles.backArrow}>
+                <Link href="/">&larr;</Link>
+            </h1>
+            <div className={styles.aboutContainer}>
+                <h1>
+                    About Me
+                </h1>
+                <p>
+                    Hey! My name is [Redacted], and I'm a [redacted] at Brown University. I'm currently a Co-Director for [Redacted Club], the club responsible for [redacted]. I'm primarily a frontend web developer, but I also have experience with UI/UX and product design work (and am an avid fan of computer graphics). Most of my time is spent thinking about aesthetics and accessibility, as well as gathering inspiration from posters and editorials!
+                </p>
+                <p>
+                    When I'm not working, I enjoy hiking, playing the New York Times Crossword, curating Spotify Playlists, playing piano, and playing The Sims 4.
+                </p>
+            </div>
+        </div>
+    );
+}
