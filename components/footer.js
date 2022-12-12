7534db3dd42e0487fc5f6253dc1ab60dc32b621c
banalbat666 <114973120+banalbat666@users.noreply.github.com>
2022-12-12 08:49:25 -0500

    Delete components directory

--- components/footer.js
+++
@@ -1,9 +0,0 @@
-import styles from "../../styles/Components.module.css";
-
-export default function Footer() {
-    return (
-        <footer className={styles.foot}>
-            © 2022 [Redacted], made with 🖤.
-        </footer>
-    );
-}
--- components/tag.js
+++
@@ -1,5 +0,0 @@
-import styles from "../../styles/Components.module.css";
-
-export default function Tag(props) {
-    return <div className={styles.box}>{props.label}</div>;
-}
